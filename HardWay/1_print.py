c402ace84f402e824993766c7128164ea9f75424
jskyzero <jskyzero@outlook.com>
2017-07-29 02:35:27 +0800

    initial

---
+++ HardWay/1_print.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do no touch this.'
+
+print "嗯，你好\n"
+print "あのう、こにちわ"
---
+++ HardWay/22_konw.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+print """
+What do you know so far
+OKOKOKOKOK
+m,,,,.....m,,,,,..,.,.,m.,k,k,k,k,k.l.l.ll.,l.l.l.l.lol.lol.llol.lol. \
+aaa
+jujmnjujmujjmik,ik,ik,ik,ik,ik,ik,ik,
+"""
